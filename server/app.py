cb9ec4ab7cd435f6d3d9308cc94c47a1256e215f
+ <+>
2023-10-22 19:41:15 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -17,34 +17,51 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    def get(self):
-        plant =Plant.query.all()
-        plant_list=[p.to_dict()for p in plant]
-        response=make_response(plant_list,200)
-        return response
-    
+     def get(self):
+        
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
 
+        response = make_response(
+            response_dict_list,
+            200,
+        )
 
-    def post(self):
-        add_plant =Plant(name =request.form.get("plant"),
-               image=request.form.get("image"),
-               price=request.form.get("price")
-               )
-        db.session.add(add_plant)
-        db.session.commit()
-        response =make_response(add_plant.to_dict(),201)
-        response.headers["Content-Type"]="application/json"
         return response
-    
-api.add_resource(Plants ,"/plants")    
+     def post(self):
+            data = request.get_json()
+            new_record = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+
+            db.session.add(new_record)
+            db.session.commit()
+
+            response_dict = new_record.to_dict()
+
+            response = make_response(
+                response_dict,
+                201,
+            )
+
+            return response
+
+api.add_resource(Plants, '/plants')
+
 class PlantByID(Resource):
-    def get(self,id):
-        plant =Plant.query.get(id).to_dict()
-        response =make_response(plant,200)
-        return response
-    
-api.add_resource(PlantByID,"/plants/<int:id>")
+    def get(self, id):
 
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
         
 
